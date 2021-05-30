a431798ae84dc7dae3d78f34df5d6b25d355b521
Summer T <summer@summertime-development.com>
2021-05-30 10:39:35 -0500

    doing some fancy stuff

--- redux/feedActions.ts
+++ redux/feedActions.ts
@@ -4,6 +4,7 @@ import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 import { Pictures } from '../types/picture';
 import { FeedObject } from '../types/feed';
+import { random } from 'lodash';
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
@@ -24,6 +25,32 @@ export const setFeed = (feedObject : string) => ({
 });
 
 
+const getEvent = (index: number) =>
+{
+    let newIndex = index;
+    const eventsArray = (newIndex : number) =>
+    {
+        const array = ['Play ball!', 
+        'Top of 1, Tokyo Lift batting.', 
+        'Yusef Fenestrate batting for the Lift.',
+    'Yusef Fenestrate hits a Single!', 
+    'Gerund Pantheocide batting for the Lift.', 
+    'Gerund Pantheocide hit a ground out to Nicholas Mora.'];
+
+
+        if(newIndex > array.length)
+        {
+            newIndex = newIndex - array.length;
+        }
+
+        return [array][newIndex];
+    }
+
+    return eventsArray(newIndex);
+}
+
+
+
 export function* asyncGetFeed(): any {
     try {
     
@@ -31,16 +58,9 @@ while(true)
 {
     yield put({
         type: feedActionsEnum.setFeed,
-        value: 'Test',
+        value: getEvent(0),
     });
-    yield delay(1000);
-
-    yield put(setFeed('???'));
-    yield delay(1000);
-  
-    yield put({
-        type: feedActionsEnum.clearFeed
-    });
+    yield delay(5000);
 }
     } catch (e) {
         throw new Error(e.message);
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,25 +1,39 @@
 import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-
+import { head } from 'lodash';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { useSelector, useDispatch } from "react-redux";
 import { callAsyncGetFeed, clearFeed } from '../redux/feedActions';
+import { Store } from '../types/store';
+import { FeedObject } from '../types/feed';
 
 export default function TabOneScreen() {
 
-  const counter = useSelector(state => state);
+  const feedState = useSelector(state => state) as Store;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(callAsyncGetFeed());
-    dispatch(clearFeed());
   }, []);
 
+
+  const lastFeed = () => {
+    if(feedState?.feed?.feedObjects)
+    {
+      return head(feedState.feed.feedObjects)?.feed;
+    }
+    else
+    {
+      return 'Booting up';
+    }
+  }
  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Fuck yeah blaseball</Text>
+      <Text style={styles.title}>{lastFeed()}</Text>
+      <Text style={styles.title}>{feedState.feed.feedObjects.length}</Text>
     </View>
   );
 }
